d9ba1cf43c146792d88d1a74822368579b2c083d
srikiran01 <srikiran0812@gmail.com>
2026-03-11 11:55:35 +0530

    Add files via upload

---
+++ week4&5/protection2.java
@@ -0,0 +1,10 @@
+package p2;
+public class Protection2 extends p1.Protection
+{
+	public Protection2()
+	{
+		System.out.println("derived other package constructor");
+		System.out.println("n_pro="+n_pro);
+		System.out.println("n_pub="+n_pub);
+	}
+}
